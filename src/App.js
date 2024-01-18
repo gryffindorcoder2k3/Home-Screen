766e5c8aab733315092502fa9f412016d1a4ffa4
Isan Agniswar Banerjee <kingagniswar@gmail.com>
2024-01-18 15:47:46 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
+const BlankBox = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="blank-box"></div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <nav className="navbar">
+        <button className="profile-button">👤</button>
+        <div className="login-signup">
+          <button className="button">Login</button>
+          <button className="button">Signup</button>
+        </div>
+      </nav>
+      <div className="content">
+        <BlankBox />
+        <section className="about-us">
+          <h2>About Us</h2>
+          <p>Welcome to our website. Learn more about us here.</p>
+        </section>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
